071173cfc20bc3cdc482b4d94b0c141d823ecff1
Adam Neumann <adam.neumann@gusto.com>
2020-03-11 15:18:03 -0600

    Static menu declaration

--- app/admin/shim.rb
+++ app/admin/shim.rb
@@ -14,11 +14,31 @@
 
 # load_resources_for_namespaces
 
+class MenuItem
+  def initialize(id, label, url)
+    @id = id
+    @label = label
+    @url = url
+  end
+
+  def options(overrides)
+    defaults.merge(overrides)
+  end
+
+  private
+  def defaults
+    {
+        id: @id, # TODO: get id from model symbol magic
+        label: @label,
+        url: @url, # TODO: Lookup magic path from model symbol here
+        if: proc { true }, # TODO: hook into cancan
+    }
+  end
+end
+
 admin = ActiveAdmin.application.namespace(:admin)
 admin.menus.on_build do
   menus = admin.menus
-  # require 'pry'; binding.pry
 
-  article_resource = Admin::ArticlesResource.resource
-  menus.add :default, article_resource.menu_item_options
+  menus.add :default, MenuItem.new('articles', 'Articles', '/admin/articles').options({ label: 'My Articles', priority: 1 })
 end
